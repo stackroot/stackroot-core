6dac5fc2a7cb3f24940ae74eac19f3e1b20135da
stackroot <stackroot@proton.me>
2025-06-03 16:13:39 +0200

    chore: reverse fix build error

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Account } from "@/components/account";
-import { ConnectOptions } from "@/components/connect-options";
+import { Account } from "@/components/Account";
+import { ConnectOptions } from "@/components/ConnectOptions";
 
 import { useAccount } from "wagmi";
 
